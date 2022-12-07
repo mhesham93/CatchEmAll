76ef28dd43c7de4963d3dc13318b2f4519dea203
Mohamed <mhesham007@hotmail.com>
2022-12-06 20:45:03 -0600

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Mohamed Said on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
